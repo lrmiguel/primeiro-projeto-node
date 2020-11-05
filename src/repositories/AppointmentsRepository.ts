da09e988ba81392e94c8e7441cdb89867a50df17
lrmiguel <leo.rmiguel@gmail.com>
2020-11-05 14:45:47 -0300

    Added TypeOrm and migrations

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -9,28 +9,12 @@ interface CreateAppointmentDTO {
 class AppointmentsRepository {
   private appointments: Appointment[];
 
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find(appointment =>
       isEqual(date, appointment.date),
     );
 
     return findAppointment || null;
-  }
-
-  public create({ provider, date }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({ provider, date });
-
-    this.appointments.push(appointment);
-
-    return appointment;
   }
 }
 
